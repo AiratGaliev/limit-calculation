9cd0e3e2c4a3ad3c290b9ae99337464f62c76de7
Airat Galiev <airat.galiev@gmail.com>
2020-07-16 17:29:04 +0300

    edited LimitCalculationApp

--- src/main/java/com/github/airatgaliev/limitcalculation/LimitCalculationApp.java
+++ src/main/java/com/github/airatgaliev/limitcalculation/LimitCalculationApp.java
@@ -1,8 +1,22 @@
 package com.github.airatgaliev.limitcalculation;
 
+import com.github.airatgaliev.limitcalculation.controller.BorrowerController;
+import com.github.airatgaliev.limitcalculation.repository.IBorrowerRepository;
+import com.github.airatgaliev.limitcalculation.repository.InMemoryBorrowerRepositoryImpl;
+import com.github.airatgaliev.limitcalculation.view.ConsoleBorrowerView;
+import com.github.airatgaliev.limitcalculation.view.ConsoleLimitView;
+import com.github.airatgaliev.limitcalculation.view.IBorrowerView;
+import com.github.airatgaliev.limitcalculation.view.ILimitView;
+
 public class LimitCalculationApp {
 
   public static void main(String[] args) {
-
+    IBorrowerRepository borrowerRepository = new InMemoryBorrowerRepositoryImpl();
+    IBorrowerView borrowerView = new ConsoleBorrowerView();
+    ILimitView limitView = new ConsoleLimitView();
+    BorrowerController borrowerController = new BorrowerController(borrowerRepository,
+        borrowerView, limitView);
+    borrowerController.createBorrowers();
+    borrowerController.getLimitInfo();
   }
 }
