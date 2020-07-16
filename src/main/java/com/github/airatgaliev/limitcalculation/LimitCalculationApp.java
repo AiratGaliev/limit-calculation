11597d21866297fd6b13ea689bf5e3c2c5ee09cd
Airat Galiev <airat.galiev@gmail.com>
2020-07-16 18:02:05 +0300

    edited LimitCalculationApp

--- src/main/java/com/github/airatgaliev/limitcalculation/LimitCalculationApp.java
+++ src/main/java/com/github/airatgaliev/limitcalculation/LimitCalculationApp.java
@@ -1,22 +1,30 @@
 package com.github.airatgaliev.limitcalculation;
 
-import com.github.airatgaliev.limitcalculation.controller.BorrowerController;
+import com.github.airatgaliev.limitcalculation.controller.LimitCalculationController;
 import com.github.airatgaliev.limitcalculation.repository.IBorrowerRepository;
 import com.github.airatgaliev.limitcalculation.repository.InMemoryBorrowerRepositoryImpl;
 import com.github.airatgaliev.limitcalculation.view.ConsoleBorrowerView;
 import com.github.airatgaliev.limitcalculation.view.ConsoleLimitView;
+import com.github.airatgaliev.limitcalculation.view.ConsoleMenuView;
 import com.github.airatgaliev.limitcalculation.view.IBorrowerView;
 import com.github.airatgaliev.limitcalculation.view.ILimitView;
+import com.github.airatgaliev.limitcalculation.view.IMenuView;
 
 public class LimitCalculationApp {
 
   public static void main(String[] args) {
-    IBorrowerRepository borrowerRepository = new InMemoryBorrowerRepositoryImpl();
-    IBorrowerView borrowerView = new ConsoleBorrowerView();
-    ILimitView limitView = new ConsoleLimitView();
-    BorrowerController borrowerController = new BorrowerController(borrowerRepository,
-        borrowerView, limitView);
-    borrowerController.createBorrowers();
-    borrowerController.getLimitInfo();
+    boolean isRestartedOrQuit = true;
+    while (isRestartedOrQuit) {
+      IBorrowerRepository borrowerRepository = new InMemoryBorrowerRepositoryImpl();
+      IBorrowerView borrowerView = new ConsoleBorrowerView();
+      ILimitView limitView = new ConsoleLimitView();
+      IMenuView menuView = new ConsoleMenuView();
+      LimitCalculationController limitCalculationController = new LimitCalculationController(
+          borrowerRepository,
+          borrowerView, limitView, menuView);
+      limitCalculationController.createBorrowers();
+      limitCalculationController.getLimitInfo();
+      isRestartedOrQuit = limitCalculationController.isRestartedCalculation();
+    }
   }
 }
